e315e73db30e116924c191ac3879c6e892292149
Rajan Maghera <rmaghera@ualberta.ca>
2022-06-02 15:23:43 -0600

    Run individual JS2 tests

--- WebDriver/drive.js
+++ WebDriver/drive.js
@@ -1,14 +1,15 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 
-const objPath = '/Users/than/Development/sm/mozilla-unified/obj-optdebug-browser-gecko-aarch64-apple-darwin21.5.0'
+const jetStreamPath = '/Users/sor-lab-02/Desktop/sm/sm/JetStream2';
+const objPath = '/Users/sor-lab-02/Desktop/sm/mozilla-unified/obj-optdebug-browser-gecko-aarch64-apple-darwin21.2.0'
 const builder = objPath + '/dist/bin/geckodriver';
 const binary = objPath + '/dist/NightlyDebug.app/Contents/MacOS/firefox';
 
 const serviceBuilder = new firefox.ServiceBuilder(builder);
 let options = new firefox.Options();
 options.setBinary(binary);
-options.addArguments('--headless');
+// options.addArguments('--headless');
 
 (async function() {
  let driver = await new Builder()
@@ -24,7 +25,16 @@ options.addArguments('--headless');
         await driver.wait(until.elementLocated(By.tagName('pre')));
         break;
       case 'jetstream':
-        await driver.get('https://browserbench.org/JetStream/');
+        const testName = process.argv[3];
+        if (!testName) {
+          console.log('running all jetstream tests');
+          await driver.get('https://browserbench.org/JetStream/');
+        } else {
+          console.log('running jetstream test: ' + testName);
+          await driver.get(`file://${jetStreamPath}/index.html`);
+          await driver.switchTo().alert().sendKeys(testName);
+          await driver.switchTo().alert().accept();
+        }
         await driver.wait(until.elementLocated(By.linkText('Start Test')));
         await driver.findElement(By.linkText('Start Test')).click();
         await driver.wait(until.elementLocated(By.className('done')));
